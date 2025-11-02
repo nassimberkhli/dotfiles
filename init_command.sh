b6afd92d5c74c1e405199a31cfb116f7db13b47f
Nassim Berkhli <91139983+nassimberkhli@users.noreply.github.com>
2025-11-02 13:16:33 +0100

    upd
    Removed SSH key generation and Git configuration commands.

--- init_command.sh
+++ init_command.sh
@@ -3,14 +3,6 @@
 systemctl --user start wireplumber pipewire pipewire-pulse
 chsh -s /usr/bin/fish
 
-cd ~
-cd .ssh
-ssh-keygen -a 100 -t ed25519
-ssh -vT git@github.com
-
-git config --global user.email "nassim.berkhli@outlook.com"
-git config --global user.name "nojo"
-
 sudo systemctl disable systemd-resolved
 sudo systemctl disable systemd-networkd
 sudo systemctl enable NetworkManager
